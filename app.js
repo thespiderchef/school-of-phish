f018573be517b288963af0466d0170ff8b3f786d
Sam Vincent <sam.vincent@students.plymouth.ac.uk>
2026-01-13 14:11:53 +0000

    feat: render hardcoded email into DOM

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // The Phisherman
-// Stage 0: JavaScript scaffold
+// got rid of stages as i lost track, will remove from all future versions
 
 console.log("The Phisherman: app.js connected");
+
+
+const emailFromEl = document.getElementById("email-from"); 
+const emailSubjectEl = document.getElementById("email-subject"); 
+const emailBodyEl = document.getElementById("email-body"); 
+
+console.log("emailFromEl:", emailFromEl);
+console.log("emailSubjectEl:", emailSubjectEl);
+console.log("emailBodyEl:", emailBodyEl);
+if (!emailFromEl || !emailSubjectEl || !emailBodyEl) {
+  console.warn("One or more email DOM elements could not be found");
+} 
